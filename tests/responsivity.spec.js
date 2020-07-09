19e2c5206c4f96eb58366eaa5f979fd2adbbdbc2
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2020-07-09 07:26:37 +0700

    add responsivity check

--- tests/responsivity.spec.js
+++ tests/responsivity.spec.js
@@ -1,8 +1,33 @@
 describe('Responsivity Test', () => {
   beforeEach(() => {
-    cy.visit('/explore');
+    cy.visit('/search?q=covid19');
   });
-  it('should hide sidebar column on mobile', () => {
-    cy.get('div[data-testid=sidebarColumn]');
+  it('should show header menu on device above 1000px screen width', () => {
+    cy.viewport('macbook-15');
+    cy.get('.HeaderMenu').should('be.visible');
+
+    cy.viewport('macbook-11');
+    cy.get('.HeaderMenu').should('be.visible');
+  });
+  it('should hide header menu below 1000px screen width', () => {
+    cy.viewport('ipad-2');
+    cy.wait(200);
+    cy.get('.HeaderMenu').should('not.be.visible');
+
+    cy.viewport('iphone-6');
+    cy.wait(200);
+    cy.get('.HeaderMenu').should('not.be.visible');
+
+    cy.viewport('iphone-5');
+    cy.wait(200);
+    cy.get('.HeaderMenu').should('not.be.visible');
+
+    cy.viewport('iphone-3');
+    cy.wait(200);
+    cy.get('.HeaderMenu').should('not.be.visible');
+
+    cy.viewport('iphone-4', 'landscape');
+    cy.wait(200);
+    cy.get('.HeaderMenu').should('not.be.visible');
   });
 });
